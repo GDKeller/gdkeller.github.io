bf2853f3fe38c6adb57d825ab21e25146ea04309
Grant Keller <grant@kellerdigital.com>
2025-04-21 22:44:48 -0700

    Install favicon plugin and add favicon file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import favicons from 'astro-favicons';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gdkeller.github.io',
   // base: 'Portfolio',
-  integrations: [tailwind()]
+  integrations: [tailwind(), favicons()]
 });
